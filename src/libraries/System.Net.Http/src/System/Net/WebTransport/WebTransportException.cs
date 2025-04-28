6391ab2f0ab8ca318ce0a2c39a9ebecd121bb992
Patrik Trefil <patrik.trefil@gmail.com>
2025-04-28 15:24:36 +0200

    rm unused empty ctor for exception

--- src/libraries/System.Net.Http/src/System/Net/WebTransport/WebTransportException.cs
+++ src/libraries/System.Net.Http/src/System/Net/WebTransport/WebTransportException.cs
@@ -4,8 +4,7 @@ namespace System.Net.WebTransport;
 
 public class WebTransportException : Exception
 {
-    public WebTransportException(): base() { }
-    public WebTransportException(string message): base(message) { }
-    public WebTransportException(string message, Exception innerException): base(message, innerException) { }
-    public WebTransportException(SerializationInfo info, StreamingContext context): base(info, context) { }
+    public WebTransportException(string message) : base(message) { }
+    public WebTransportException(string message, Exception innerException) : base(message, innerException) { }
+    public WebTransportException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
