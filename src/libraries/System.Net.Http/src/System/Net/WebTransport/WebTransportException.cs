2152c7f5d9af17529eaa8adbe0bbfd3bf42d267e
Patrik Trefil <patrik.trefil@gmail.com>
2025-06-16 17:22:27 +0200

    exception class hierarchy design

--- src/libraries/System.Net.Http/src/System/Net/WebTransport/WebTransportException.cs
+++ src/libraries/System.Net.Http/src/System/Net/WebTransport/WebTransportException.cs
@@ -5,6 +5,35 @@ namespace System.Net.WebTransport;
 
 public class WebTransportException : Exception
 {
-    public WebTransportException(string message) : base(message) { }
-    public WebTransportException(string message, Exception innerException) : base(message, innerException) { }
+    internal WebTransportException(string message) : base(message) { }
+    internal WebTransportException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public sealed class WebTransportStreamClosedException : WebTransportException
+{
+    public int ApplicationErrorCode { get; }
+    internal WebTransportStreamClosedException(string message, int applicationErrorCode) : base(message)
+    {
+        ApplicationErrorCode = applicationErrorCode;
+    }
+    internal WebTransportStreamClosedException(string message, int applicationErrorCode, Exception innerException) : base(message, innerException)
+    {
+        ApplicationErrorCode = applicationErrorCode;
+    }
+}
+
+public sealed class WebTransportSessionClosedException : WebTransportException
+{
+    public int ApplicationErrorCode { get; }
+    public string ApplicationErrorMessage { get; }
+    internal WebTransportSessionClosedException(string message, int applicationErrorCode, string applicationErrorMessage) : base(message)
+    {
+        ApplicationErrorCode = applicationErrorCode;
+        ApplicationErrorMessage = applicationErrorMessage;
+    }
+    internal WebTransportSessionClosedException(string message, int applicationErrorCode, string applicationErrorMessage, Exception innerException) : base(message, innerException)
+    {
+        ApplicationErrorCode = applicationErrorCode;
+        ApplicationErrorMessage = applicationErrorMessage;
+    }
 }
