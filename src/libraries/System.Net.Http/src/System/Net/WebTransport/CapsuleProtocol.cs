82f85a092e066dec61a3288834674199ff0187ff
Patrik Trefil <patrik.trefil@gmail.com>
2025-07-06 15:36:39 +0200

    implement Capsule protocol with buffers

--- src/libraries/System.Net.Http/src/System/Net/WebTransport/CapsuleProtocol.cs
+++ src/libraries/System.Net.Http/src/System/Net/WebTransport/CapsuleProtocol.cs
@@ -7,16 +7,32 @@ using System.Text;
 using System.Threading;
 using System.Buffers.Binary;
 using System.Diagnostics;
+using System.Net.Http;
 
 namespace System.Net.WebTransport;
 
+// TODO: reimplement serialization with buffers?
+
 // The capsule protocol can be found here https://datatracker.ietf.org/doc/html/rfc9297
 
-internal sealed class CloseSessionCapsule
+internal abstract class Capsule
+{
+    /// <summary>
+    /// Processes the capsule received from the peer.
+    /// </summary>
+    /// <param name="session">WebTransport session which received the session.</param>
+    public abstract void ProcessReceived(WebTransportSession session);
+}
+
+internal sealed class CloseSessionCapsule : Capsule
 {
     public const long CapsuleCode = 0x2843;
-    private static readonly Encoding _encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
-    private const int ApplicationErrorMessageLengthInBytesLimit = 8192;
+    private static readonly Encoding _encoding = Encoding.UTF8;
+
+    private const int s_applicationErrorCodeSize = sizeof(uint);
+    private const int s_applicationErrorMessageOffset = s_applicationErrorCodeSize;
+    private const int s_applicationErrorMessageLengthInBytesLimit = 8192;
+
     public uint ApplicationErrorCode { get; }
     /// <summary>
     /// Utf-8 encoded string containing the error message.
@@ -27,27 +43,35 @@ internal sealed class CloseSessionCapsule
         ApplicationErrorCode = applicationErrorCode;
         ApplicationErrorMessage = applicationErrorMessage;
     }
-    public void Process(WebTransportSession session)
+    public override void ProcessReceived(WebTransportSession session)
     {
         string applicationErrorMessageString = _encoding.GetString(ApplicationErrorMessage.Span);
         session.ReceiveClose(ApplicationErrorCode, applicationErrorMessageString);
     }
-    public static async Task<CloseSessionCapsule> DeserializeAsync(Stream stream, CancellationToken cancellationToken = default)
+    public static CloseSessionCapsule Deserialize(ReadOnlyMemory<byte> buffer)
     {
-        long length = VariableLengthIntegerStreamHelper.Read(stream);
-        long applicationErrorMessageLength = length - 4;
-        if (applicationErrorMessageLength > ApplicationErrorMessageLengthInBytesLimit)
+        if (buffer.Length < s_applicationErrorCodeSize)
+        {
+            throw new WebTransportException("Invalid connect stream data received");
+        }
+
+        long applicationErrorMessageLength = buffer.Length - s_applicationErrorMessageOffset;
+
+        if (applicationErrorMessageLength > s_applicationErrorMessageLengthInBytesLimit)
         {
             throw new WebTransportException("Application error message length exceeded");
         }
-        Debug.Assert(ApplicationErrorMessageLengthInBytesLimit + 4 < sizeof(int));
-        int intLength = (int)length;
-        byte[] buffer = new byte[intLength];
-        await stream.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
-        bool isSuccess = BinaryPrimitives.TryReadUInt32BigEndian(buffer, out uint applicationErrorCode);
-        Debug.Assert(isSuccess, $"{nameof(buffer)} should be large enough");
-        ReadOnlyMemory<byte> applicationErrorMessage = new ArraySegment<byte>(buffer, 4, buffer.Length - 4);
-        return new CloseSessionCapsule(applicationErrorCode, applicationErrorMessage);
+
+        bool isReadOfApplicationErrorCodeSuccessful = BinaryPrimitives.TryReadUInt32BigEndian(buffer.Span, out uint applicationErrorCode);
+        if (!isReadOfApplicationErrorCodeSuccessful)
+        {
+            throw new WebTransportException("Invalid connect stream data received");
+        }
+
+        byte[] errorMessageBuffer = new byte[applicationErrorMessageLength];
+        buffer.Slice(s_applicationErrorMessageOffset).CopyTo(errorMessageBuffer);
+
+        return new CloseSessionCapsule(applicationErrorCode, errorMessageBuffer);
     }
     public async Task SerializeAsync(Stream stream, CancellationToken cancellationToken = default)
     {
@@ -64,31 +88,31 @@ internal sealed class CloseSessionCapsule
     }
 }
 
-internal sealed class DrainSessionCapsule
+internal sealed class DrainSessionCapsule : Capsule
 {
     public const long CapsuleCode = 0x78ae;
     public DrainSessionCapsule() { }
-    [Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Keep the same interface for all capsule classes")]
-    public async Task ProcessAsync(WebTransportSession session, CancellationToken cancellationToken = default)
+    public override void ProcessReceived(WebTransportSession session)
     {
-        await session.CloseAsync(cancellationToken).ConfigureAwait(false);
+        session.ReceiveDrain();
     }
-    public static DrainSessionCapsule Deserialize(Stream stream) {
-        long length = VariableLengthIntegerStreamHelper.Read(stream);
-        if (length != 0)
+    public static DrainSessionCapsule Deserialize(ReadOnlyMemory<byte> buffer)
+    {
+        if (buffer.Length != 0)
         {
-            throw new WebTransportException($"Invalid capsule length {length} bytes");
+            throw new WebTransportException("Invalid capsule data received");
         }
         return new DrainSessionCapsule();
     }
     [Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Keep the same interface for all capsule classes")]
-    public void Serialize(Stream stream) {
+    public void Serialize(Stream stream)
+    {
         VariableLengthIntegerStreamHelper.Write(stream, CapsuleCode);
         VariableLengthIntegerStreamHelper.Write(stream, 0);
     }
 }
 
-internal sealed class MaxBidirectionalStreamsCapsule
+internal sealed class MaxBidirectionalStreamsCapsule : Capsule
 {
     public const long CapsuleCode = 0x190B4D3F;
     public long MaxBidirectionalStreams { get; }
@@ -96,24 +120,27 @@ internal sealed class MaxBidirectionalStreamsCapsule
     {
         MaxBidirectionalStreams = maxBidirectionalStreams;
     }
-    public void Process(WebTransportSession session)
+    public override void ProcessReceived(WebTransportSession session)
     {
-        session.BidirectionalStreamCountLimitForPeer = MaxBidirectionalStreams;
+        session.BidirectionalStreamCountLimitProvidedByPeer = MaxBidirectionalStreams;
     }
     /// <exception cref="WebTransportException">When the received length does not match the payload length</exception>
-    public static MaxBidirectionalStreamsCapsule Deserialize(Stream stream) {
-        long length = VariableLengthIntegerStreamHelper.Read(stream);
-        long maxBidirectionalStream = VariableLengthIntegerStreamHelper.Read(stream, out int maxBidirectionalStreamsBytesRead);
-        if (length != maxBidirectionalStreamsBytesRead)
+    public static MaxBidirectionalStreamsCapsule Deserialize(ReadOnlyMemory<byte> buffer)
+    {
+        bool isReadSuccessful = VariableLengthIntegerHelper.TryRead(buffer.Span, out long maxBidirectionalStreams, out int bytesRead);
+
+        if (!isReadSuccessful || buffer.Length != bytesRead)
         {
-            throw new WebTransportException("Deserialization failed because of invalid capsule data - received length does not match the payload length")
+            throw new WebTransportException("Received invalid capsule data");
         }
-        return new MaxBidirectionalStreamsCapsule(maxBidirectionalStream);
+
+        return new MaxBidirectionalStreamsCapsule(maxBidirectionalStreams);
     }
-    public void Serialize(Stream stream) {
+    public void Serialize(Stream stream)
+    {
         VariableLengthIntegerStreamHelper.Write(stream, CapsuleCode);
         Span<byte> buffer = stackalloc byte[VariableLengthIntegerStreamHelper.MaximumEncodedLength];
-        bool isSuccess = System.Net.Http.VariableLengthIntegerHelper.TryWrite(buffer, MaxBidirectionalStreams, out int bytesWritten);
+        bool isSuccess = VariableLengthIntegerHelper.TryWrite(buffer, MaxBidirectionalStreams, out int bytesWritten);
         Debug.Assert(isSuccess, $"{nameof(buffer)} should be large enough");
         for (int i = 0; i < bytesWritten; i++)
         {
@@ -122,7 +149,7 @@ internal sealed class MaxBidirectionalStreamsCapsule
     }
 }
 
-internal sealed class MaxUnidirectionalStreamsCapsule
+internal sealed class MaxUnidirectionalStreamsCapsule : Capsule
 {
     public const long CapsuleCode = 0x190B4D40;
     public long MaxUnidirectionalStreams { get; }
@@ -130,24 +157,27 @@ internal sealed class MaxUnidirectionalStreamsCapsule
     {
         MaxUnidirectionalStreams = maxUnidirectionalStreams;
     }
-    public void Process(WebTransportSession session)
+    public override void ProcessReceived(WebTransportSession session)
     {
-        session.UnidirectionalStreamCountLimitForPeer = MaxUnidirectionalStreams;
+        session.UnidirectionalStreamCountLimitProvidedByPeer = MaxUnidirectionalStreams;
     }
     /// <exception cref="WebTransportException">When the received length does not match the payload length</exception>
-    public static MaxUnidirectionalStreamsCapsule Deserialize(Stream stream) {
-        long length = VariableLengthIntegerStreamHelper.Read(stream);
-        long maxUnidirectionalStream = VariableLengthIntegerStreamHelper.Read(stream, out int maxUnidirectionalStreamsBytesRead);
-        if (length != maxUnidirectionalStreamsBytesRead)
+    public static MaxUnidirectionalStreamsCapsule Deserialize(ReadOnlyMemory<byte> buffer)
+    {
+        bool isReadSuccessful = VariableLengthIntegerHelper.TryRead(buffer.Span, out long maxUnidirectionalStream, out int maxUnidirectionalStreamsBytesRead);
+
+        if (!isReadSuccessful || buffer.Length != maxUnidirectionalStreamsBytesRead)
         {
-            throw new WebTransportException("Deserialization failed because of invalid capsule data - received length does not match the payload length")
+            throw new WebTransportException("Deserialization failed because of invalid capsule data - received length does not match the payload length");
         }
+
         return new MaxUnidirectionalStreamsCapsule(maxUnidirectionalStream);
     }
-    public void Serialize(Stream stream) {
+    public void Serialize(Stream stream)
+    {
         VariableLengthIntegerStreamHelper.Write(stream, CapsuleCode);
         Span<byte> buffer = stackalloc byte[VariableLengthIntegerStreamHelper.MaximumEncodedLength];
-        bool isSuccess = System.Net.Http.VariableLengthIntegerHelper.TryWrite(buffer, MaxUnidirectionalStreams, out int bytesWritten);
+        bool isSuccess = VariableLengthIntegerHelper.TryWrite(buffer, MaxUnidirectionalStreams, out int bytesWritten);
         Debug.Assert(isSuccess, $"{nameof(buffer)} should be large enough");
         for (int i = 0; i < bytesWritten; i++)
         {
@@ -156,7 +186,7 @@ internal sealed class MaxUnidirectionalStreamsCapsule
     }
 }
 
-internal sealed class MaxDataCapsule
+internal sealed class MaxDataCapsule : Capsule
 {
     public const long CapsuleCode = 0x190B4D3D;
     public long MaxData { get; }
@@ -164,24 +194,27 @@ internal sealed class MaxDataCapsule
     {
         MaxData = maxData;
     }
-    public void Process(WebTransportSession session)
+    public override void ProcessReceived(WebTransportSession session)
     {
-        session.MaxDataSentLimitForPeer = MaxData;
+        session.MaxDataSentLimitProvidedByPeer = MaxData;
     }
     /// <exception cref="WebTransportException">When the received length does not match the payload length</exception>
-    public static MaxDataCapsule Deserialize(Stream stream) {
-        long length = VariableLengthIntegerStreamHelper.Read(stream);
-        long maxData = VariableLengthIntegerStreamHelper.Read(stream, out int maxDataBytesRead);
-        if (length != maxDataBytesRead)
+    public static MaxDataCapsule Deserialize(ReadOnlyMemory<byte> buffer)
+    {
+        bool isReadSuccessful = VariableLengthIntegerHelper.TryRead(buffer.Span, out long maxData, out int maxDataBytesRead);
+
+        if (!isReadSuccessful || buffer.Length != maxDataBytesRead)
         {
-            throw new WebTransportException("Deserialization failed because of invalid capsule data - received length does not match the payload length")
+            throw new WebTransportException("Received invalid capsule data");
         }
+
         return new MaxDataCapsule(maxData);
     }
-    public void Serialize(Stream stream) {
+    public void Serialize(Stream stream)
+    {
         VariableLengthIntegerStreamHelper.Write(stream, CapsuleCode);
         Span<byte> buffer = stackalloc byte[VariableLengthIntegerStreamHelper.MaximumEncodedLength];
-        bool isSuccess = System.Net.Http.VariableLengthIntegerHelper.TryWrite(buffer, MaxData, out int bytesWritten);
+        bool isSuccess = VariableLengthIntegerHelper.TryWrite(buffer, MaxData, out int bytesWritten);
         Debug.Assert(isSuccess, $"{nameof(buffer)} should be large enough");
         for (int i = 0; i < bytesWritten; i++)
         {
@@ -190,44 +223,74 @@ internal sealed class MaxDataCapsule
     }
 }
 
+/// <summary>
+/// Reads and processes capsules from a capsule stream.
+/// </summary>
 internal sealed class CapsuleConsumer
 {
     private readonly Stream _capsuleStream;
     private readonly WebTransportSession _session;
+    private readonly ArrayBuffer _buffer = new(initialSize: VariableLengthIntegerHelper.MaximumEncodedLength, usePool: true);
     public CapsuleConsumer(Stream capsuleStream, WebTransportSession session)
     {
         _capsuleStream = capsuleStream;
         _session = session;
+    }
+
+    private async ValueTask<long> ReadVariableLengthIntegerAsync(CancellationToken cancellationToken = default)
+    {
+        int bytesRead;
+        long value;
+        while (!VariableLengthIntegerHelper.TryRead(_buffer.ActiveSpan, out value, out bytesRead))
+        {
+            _buffer.EnsureAvailableSpace(VariableLengthIntegerHelper.MaximumEncodedLength);
+            bytesRead = await _capsuleStream.ReadAsync(_buffer.AvailableMemory, cancellationToken).ConfigureAwait(false);
+
+            if (bytesRead == 0)
+            {
+                throw new WebTransportSessionClosedException("Session has been closed by peer", 0, "");
+            }
+            _buffer.Commit(bytesRead);
+        }
+
+        _buffer.Discard(bytesRead);
+
+        return value;
     }
     /// <summary>Processes the next capsule in the provided capsule stream.
     /// If an unknown capsule type is received, the capsule is dropped and the call ends.</summary>
     /// <exception cref="ObjectDisposedException">When the capsule stream or the session have been disposed.</exception>
     public async Task ProcessNextCapsule(CancellationToken cancellationToken = default)
     {
-        long incomingType = VariableLengthIntegerStreamHelper.Read(_capsuleStream);
-        switch (incomingType)
+
+        long capsuleType = await ReadVariableLengthIntegerAsync(cancellationToken).ConfigureAwait(false);
+        long capsuleLength = await ReadVariableLengthIntegerAsync(cancellationToken).ConfigureAwait(false);
+        int capsuleLengthInt;
+        try
         {
-            case CloseSessionCapsule.CapsuleCode:
-                CloseSessionCapsule closeSessionCapsule = await CloseSessionCapsule.DeserializeAsync(_capsuleStream, cancellationToken).ConfigureAwait(false);
-                closeSessionCapsule.Process(_session);
-                break;
-            case DrainSessionCapsule.CapsuleCode:
-                DrainSessionCapsule drainSessionCapsule = DrainSessionCapsule.Deserialize(_capsuleStream);
-                await drainSessionCapsule.ProcessAsync(_session, cancellationToken).ConfigureAwait(false);
-                break;
-            case MaxBidirectionalStreamsCapsule.CapsuleCode:
-                MaxBidirectionalStreamsCapsule maxBidirectionalStreamsCapsule = MaxBidirectionalStreamsCapsule.Deserialize(_capsuleStream);
-                maxBidirectionalStreamsCapsule.Process(_session);
-                break;
-            case MaxUnidirectionalStreamsCapsule.CapsuleCode:
-                MaxUnidirectionalStreamsCapsule maxUnidirectionalStreamsCapsule = MaxUnidirectionalStreamsCapsule.Deserialize(_capsuleStream);
-                maxUnidirectionalStreamsCapsule.Process(_session);
-                break;
-            case MaxDataCapsule.CapsuleCode:
-                MaxDataCapsule maxDataCapsule = MaxDataCapsule.Deserialize(_capsuleStream);
-                maxDataCapsule.Process(_session);
-                break;
+            capsuleLengthInt = checked((int)capsuleLength);
         }
+        catch (OverflowException)
+        {
+            throw new WebTransportException("Unknown capsule received"); // All known capsule lengths are less than int.MaxValue
+        }
+
+        _buffer.EnsureAvailableSpace(capsuleLengthInt);
+        await _capsuleStream.ReadExactlyAsync(_buffer.AvailableMemory, cancellationToken).ConfigureAwait(false);
+
+        Capsule? capsule = capsuleType switch
+        {
+            CloseSessionCapsule.CapsuleCode => CloseSessionCapsule.Deserialize(_buffer.ActiveMemory),
+            DrainSessionCapsule.CapsuleCode => DrainSessionCapsule.Deserialize(_buffer.ActiveMemory),
+            MaxBidirectionalStreamsCapsule.CapsuleCode => MaxBidirectionalStreamsCapsule.Deserialize(_buffer.ActiveMemory),
+            MaxUnidirectionalStreamsCapsule.CapsuleCode => MaxUnidirectionalStreamsCapsule.Deserialize(_buffer.ActiveMemory),
+            MaxDataCapsule.CapsuleCode => MaxDataCapsule.Deserialize(_buffer.ActiveMemory),
+            _ => null,
+        };
         // Unknown capsules are silently dropped https://datatracker.ietf.org/doc/html/rfc9297#section-3.2-7
+
+        _buffer.ClearAndReturnBuffer();
+
+        capsule?.ProcessReceived(_session);
     }
 }
