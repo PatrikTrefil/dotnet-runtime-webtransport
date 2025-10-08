3e8a271293a86db0b895cad3c35d31e4d31a752d
Patrik Trefil <patrik.trefil@gmail.com>
2025-10-08 09:56:32 +0200

    set higher open streams limits for quic connections used for HTTP/3 requests
    Instead of providing users a way to configure these settings, we decided to only increase the limit by a large amount for now.

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/ConnectHelper.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/ConnectHelper.cs
@@ -120,9 +120,9 @@ namespace System.Net.Http
             {
                 return await QuicConnection.ConnectAsync(new QuicClientConnectionOptions()
                 {
-                    // TODO: user should be able to configure these limits
-                    MaxInboundBidirectionalStreams = 5, // Needed for WebTransport
-                    MaxInboundUnidirectionalStreams = 5, // Minimum is 3: https://www.rfc-editor.org/rfc/rfc9114.html#unidirectional-streams (1x control stream + 2x QPACK). Set to 100 if/when support for PUSH streams is added.
+                    // MaxInbound*Streams is set to a high value for WebTransport - if need be, we can give the user a way to configure this.
+                    MaxInboundBidirectionalStreams = 150,
+                    MaxInboundUnidirectionalStreams = 150, // Minimum is 3: https://www.rfc-editor.org/rfc/rfc9114.html#unidirectional-streams (1x control stream + 2x QPACK). Set to 100 if/when support for PUSH streams is added.
                     IdleTimeout = idleTimeout,
                     DefaultStreamErrorCode = (long)Http3ErrorCode.RequestCancelled,
                     DefaultCloseErrorCode = (long)Http3ErrorCode.NoError,
